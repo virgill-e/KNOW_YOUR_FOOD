7e2a96e96494f2a7c716b4efdd38b92fb4cdc308
Virgill_e <virgile.bigare@gmail.com>
2025-11-25 16:37:52 +0100

    footer and header responsive

--- app/components/layout/footer.vue
+++ app/components/layout/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex flex-row justify-between items-center py-8 px-16">
+  <footer class="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-0 py-4 md:py-8 px-4 md:px-8 lg:px-16 text-center md:text-left">
     <span>Let's eat smarter together</span>
       <span>Thank to <nuxt-link class="underline underline-offset-2" target="_blank" to="https://world.openfoodfacts.org/">Open Food Facts</nuxt-link> for data</span>
   </footer>
--- app/components/layout/header.vue
+++ app/components/layout/header.vue
@@ -1,9 +1,9 @@
 <template>
-  <header class="flex flex-row justify-between items-center py-8 px-16">
-    <h1 class="text-7xl font-bold">KNOW YOUR FOOD</h1>
-    <div class="flex flex-row justify-between gap-4">
-      <nuxt-link target="_blank" to="https://www.virgill-e.com/">Contact</nuxt-link>
-      <nuxt-link target="_blank" to="https://github.com/virgill-e/know_your_food">Github</nuxt-link>
+  <header class="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0 py-4 md:py-8 px-4 md:px-8 lg:px-16">
+    <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold text-center md:text-left">KNOW YOUR FOOD</h1>
+    <div class="flex flex-row justify-center md:justify-between gap-4">
+      <nuxt-link target="_blank" to="https://www.virgill-e.com/" class="hover:underline">Contact</nuxt-link>
+      <nuxt-link target="_blank" to="https://github.com/virgill-e/know_your_food" class="hover:underline">Github</nuxt-link>
     </div>
   </header>
 </template>
