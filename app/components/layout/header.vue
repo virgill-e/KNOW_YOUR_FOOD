7bec5aa7f3a586303c96b169509a2715304c6740
Virgill_e <virgile.bigare@gmail.com>
2025-11-25 15:39:28 +0100

    link header

--- app/components/layout/header.vue
+++ app/components/layout/header.vue
@@ -2,8 +2,8 @@
   <header class="flex flex-row justify-between items-center py-8 px-16">
     <h1 class="text-7xl font-bold">KNOW YOUR FOOD</h1>
     <div class="flex flex-row justify-between gap-4">
-      <nuxt-link to="/">Contact</nuxt-link>
-      <nuxt-link to="/">Github</nuxt-link>
+      <nuxt-link target="_blank" to="https://www.virgill-e.com/">Contact</nuxt-link>
+      <nuxt-link target="_blank" to="https://github.com/virgill-e/know_your_food">Github</nuxt-link>
     </div>
   </header>
 </template>
